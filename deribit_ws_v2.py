971f02e132b7a4e34a628c3f82f716e9019dbc13
pigerloafer <kruj1998@gmail.com>
2021-07-29 18:07:59 +0300

    Added real time basis

--- deribit_ws_v2.py
+++ deribit_ws_v2.py
@@ -1,30 +1,84 @@
-import websocket, json
-socket = 'wss://www.deribit.com/ws/api/v2'
+import asyncio
+import websockets
+import json
+import time
+import logging
+import os
+from helpers import * 
 
-def on_open(ws):
-    print("Opened")
-    data = {
-    "method": "public/subscribe",
-    "params": {
-    "channels": [
-        "quote.ETH-PERPETUAL",
-    ]
-    },
-    "data": "ETH-PERPETUAL",
-    "jsonrpc": "2.0",
-    "id": 15
-}
+class SimpleBasisBot:
+    
+    def __init__(self, parameters, test="False"):
+        self.parameters = parameters
+        if test:
+            self.uri = "wss://www.deribit.com/ws/api/v2"
+        else:
+            self.uri = 'wss://test.deribit.com/ws/api/v2' 
+        self.is_working = False
 
-    ws.send(json.dumps(data))
+        logging.basicConfig(level=logging.INFO, filename=f"SimpleBasisBot_{self.parameters['name']}",
+                    format='%(asctime)s  %(levelname)s:  %(message)s' )
+        self.log = logging.getLogger(__name__)
+        self.log.info(f"Creating SimpleBasisBot with params {get_parameters_table(parameters, '')}")
 
+    async def start(self):
+        self.log.info(f"Starting bot with name {self.parameters['name']}")
+        self.basis = None
+        self.last_base_price = None
+        self.last_other_price = None
+        async with websockets.connect(self.uri) as websocket:
+            self.log.info(f"Socker is opened")
+            data = {
+                "method": "public/subscribe",
+                "params": {
+                "channels": [
+                f"quote.{self.parameters['base_inst']}",
+                f"quote.{self.parameters['other_inst']}"
+                ]
+                },
+                "jsonrpc": "2.0",
+                "id": 1
+            }
 
-def on_message(ws, message):
-    data = json.loads(message)
-    # print(data)
-    print("="*50)
-    print(f'best_bid_price: {data["params"]["data"]["best_bid_price"]}\nbest_ask_price: {data["params"]["data"]["best_ask_price"]}')
-    print("="*50)
+            await websocket.send(json.dumps(data))
+            while(websocket.open):
+                self.calculating_basis(await websocket.recv())
 
-ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
+    def calculating_basis(self, message):
+        if "params" in message:
+            data = json.loads(message)["params"]["data"]
+            if data["instrument_name"] == self.parameters["base_inst"]:
+                if self.parameters["base_side"] == "sell":
+                    self.last_base_price = data["best_ask_price"]
+                else:
+                    self.last_base_price = data["best_bid_price"]
+            else:
+                if self.parameters["other_side"] == "sell":
+                    self.last_other_price = data["best_ask_price"]
+                else:
+                    self.last_other_price = data["best_bid_price"]
+            
+            if self.last_base_price is not None and self.last_other_price is not None:
+                self.basis = self.last_base_price - self.last_other_price
+        
+            if self.basis is not None:
+                clear = lambda: os.system('clear')
+                clear()
+                print(f"Last_base_price: {self.last_base_price}\nLast_other_price: {self.last_other_price}\nBasis: {self.basis}")
 
-ws.run_forever()
+def main():
+    bot = SimpleBasisBot({
+        "name": "Test",
+        "base_inst": "ETH-PERPETUAL",
+        "other_inst": "ETH-24SEP21",
+        "base_side": "buy",
+        "other_side": "sell",
+        "base_amount": 10. ,
+        "other_amount": 10. ,
+        "basis": 15,
+    })
+    asyncio.get_event_loop().run_until_complete(bot.start())
+
+if __name__ == "__main__":
+    main()
+
